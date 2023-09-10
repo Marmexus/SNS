691af1a28bc96b3f4725210741c8a5cef3cc7d8f
Nattapong Nantasang <nattapong.nan@kkumail.com>
2023-09-10 16:12:46 +0700

    hashing register password

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,6 +1,12 @@
 import UserModel from '../models';
 import { Request, Response } from 'express';
 import { registerValidator } from '../middlewares';
+import bcrypt from 'bcrypt';
+
+function passwordEncrypt(password: string) {
+    const salt: number = 10;
+    return bcrypt.hash(password, salt);
+}
 
 export async function register(req: Request, res: Response): Promise<any> {
     const { username, name, email, password } = req.body;
@@ -10,18 +16,20 @@ export async function register(req: Request, res: Response): Promise<any> {
         return res.status(400).json(validated.error.details[0].message);
     }
 
-    const existedUser = await UserModel.find({username});
-    if (existedUser.length > 0){
+    const existedUser = await UserModel.find({ username });
+    if (existedUser.length > 0) {
         return res.status(400).json('User existed');
     }
+
+    const passwordEncrypted: string = await passwordEncrypt(password);
 
     const user = new UserModel({
         username,
         name,
         email,
-        password
+        password: passwordEncrypted
     })
 
-    // await user.save();
+    await user.save();
     return res.status(201).json(user);
 }
