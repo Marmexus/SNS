2327970a3774c17ac11a1e6c427d191265bb599d
Nattapong Nantasang <nattapong.nan@kkumail.com>
2023-09-10 17:15:10 +0700

    delete login validator

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,3 +1,3 @@
-export { registerValidator, loginValidator } from "./validator";
+export { registerValidator } from "./validator";
 export { authorize } from './authorization';
 export { createToken } from './auth/jwt'
